7c193f797162e2eb5cccc911d0f453251ab658c4
Collin  Jones <collinpjones@users.noreply.github.com>
2016-10-30 15:20:39 -0500

    Added history

--- app/controllers/guesses_controller.rb
+++ app/controllers/guesses_controller.rb
@@ -2,6 +2,12 @@ class GuessesController < ApplicationController
   def index
     #params = {"first_number"=>"1", "second_number"=>"2", "third_number"=>"3"}
 
+    g = Guess.new
+    g.first_num = params["first_number"]
+    g.second_num = params["second_number"]
+    g.third_num = params["third_number"]
+    g.save
+    
     @list = Guess.all
 
     render ("guesses/index.html.erb")
